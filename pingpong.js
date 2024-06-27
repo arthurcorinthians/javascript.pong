baf283ec3115460dfb5353b756688c82e1783919
arthurcorinthians <168776336+arthurcorinthians@users.noreply.github.com>
2024-06-27 17:41:08 -0300

    cria bolinha

--- pingpong.js
+++ pingpong.js
@@ -1,3 +1,7 @@
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
 function setup () {
     //aqui vou criar minha "mesa"
         createCanvas (600,400);
@@ -8,5 +12,10 @@ function draw (){
     //a cor da mesa é em rgb
     background (75, 0, 130);
 
+    criaBolinha (xBolinha, yBolinha, tamBolinha)
 }
 
+//funcao bolinha
+function criaBolinha (Xbolinha, yBolinha, tamBolinha){
+    circle (xBolinha, yBolinha, tamBolinha);
+}
