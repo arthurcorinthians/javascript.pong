33a536dd396ba61cfdff727f1f450d5ca72e2fde
arthurcorinthians <168776336+arthurcorinthians@users.noreply.github.com>
2024-08-01 15:42:59 -0300

    .

--- pingpong.js
+++ pingpong.js
@@ -7,6 +7,12 @@ let tamBolinha = 25;
 let xvelocidadeBolinha = 6;
 let yvelocidadeBolinha = 6;
 
+//aqui estou configurando a raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let larguraRaquete = 10;
+let alturaRaquete = 90;
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -23,6 +29,12 @@ function draw(){
     moveBolinha();
     //chamando a borda
     Borda();
+ 
+    //criandoRaquete
+    criaRaquete (xRaquete, yRaquete, larguraRaquete, alturaRaquete);
+    moveRaquete()
+    colideRaquete();
+
 }
 
 //função bolinha
@@ -44,3 +56,26 @@ function Borda(){
         yvelocidadeBolinha *= -1;
     }
 }
+//funcao responsavel por criar o retangulo que representa a raquete
+function criaRaquete (xRaquete, yRaquete, larguraRaquete, alturaRaquete) {
+    fill ("blue");
+    rect (xRaquete, yRaquete, larguraRaquete, alturaRaquete);
+}
+//funçao responsavel por movimentar a raquete
+function movimentaRaquete(){
+
+    if(keyIsDown(UP_ARROW)){
+        yRaquete -= 10;
+    }
+    if(keyIsDown(DOWN_ARROW)){
+        yRaquete += 10;
+    }
+}
+//funçao responsavel por quando a bolinha bater na raquete, retornar em direçao contraria
+function colideRaquete(){
+
+    if (xBolinha - raioBolinha < xRaquete + larguraRaquete && yBolinha - raioBolinha < yRaquete + alturaRaquete && yBolinha + raioBolinha > yRaquete){
+
+        xvelocidadeBolinha *= -1;
+    }
+    }
