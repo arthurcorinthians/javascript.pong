3c00ef4c1a5194c3ae7156028aa4b2a2b64641b5
arthurcorinthians <168776336+arthurcorinthians@users.noreply.github.com>
2024-08-01 16:40:49 -0300

    .

--- pingpong.js
+++ pingpong.js
@@ -1,75 +1,72 @@
-//aqui eu vou criar a bolinha
 let xBolinha = 300;
 let yBolinha = 200;
 let tamBolinha = 25;
 let raioBolinha = tamBolinha/2;
 
-//config velocidade bolinha
+
 let xvelocidadeBolinha = 6;
 let yvelocidadeBolinha = 6;
 
-//aqui estou configurando a raquete
-let xRaquete = 5;
-let yRaquete = 150;
-let larguraRaquete = 10;
-let alturaRaquete = 90;
+let xraquete = 5;
+let yraquete = 150;
+let larguraraquete = 10;
+let alturaraquete = 90;
 
 function setup(){
-    //aqui vou criar minha "mesa"
+   
     createCanvas(600,400);
 }
 
 function draw(){
-    //função responsável pelo "desenho e animação da mesa"
-    //aqui vou por a cor da "mesa"
-    //A cor da mesa é em rgb
+   
     background(128,128,0); 
-    //chamando a função cria bolinha para criar a bolinha
     criaBolinha(xBolinha, yBolinha, tamBolinha);
-    //chamando a função mov bolinha
     moveBolinha();
-    //chamando a borda
     bolinhaBorda();
- 
-    //criandoRaquete
-    criaRaquete (xRaquete, yRaquete, larguraRaquete, alturaRaquete);
-    moveRaquete()
-    colideRaquete();
-
+    criaraquete(xraquete, yraquete, larguraraquete, alturaraquete);
+    movimentaraquete();
+    colideraquete();
 }
 
-//função bolinha
-function criaBolinha(xBolinha, yBolinha,tamBolinha){
-    fill ("red");
-    circle (xBolinha,yBolinha,tamBolinha);
+// função bolinha
+function criaBolinha(xBolinha, yBolinha, tamBolinha){
+    fill("red"); 
+    circle (xBolinha, yBolinha, tamBolinha);
 }
 
-//função move bolinha
+// função move bolinha
 function moveBolinha(){
-    xBolinha = xvelocidadeBolinha + xBolinha;
-    yBolinha = yvelocidadeBolinha + yBolinha;
+    xBolinha += xvelocidadeBolinha;
+    yBolinha += yvelocidadeBolinha;
 }
 
-//funcao responsavel por criar o retangulo que representa a raquete
-function criaRaquete (xRaquete, yRaquete, larguraRaquete, alturaRaquete) {
-    fill ("blue");
-    rect(xRaquete, yRaquete, larguraRaquete, alturaRaquete);
-}
-//funçao responsavel por movimentar a raquete
-function movimentaRaquete(){
-
-    if(keyIsDown(UP_ARROW)){
-        yRaquete -= 10;
+function bolinhaBorda(){ 
+    if (xBolinha > width || xBolinha < 0){
+        xvelocidadeBolinha *= -1;
     }
-    if(keyIsDown(DOWN_ARROW)){
-        yRaquete += 10;
+    if (yBolinha > height || yBolinha < 0){
+        yvelocidadeBolinha *= -1;
     }
 }
-//funçao responsavel por quando a bolinha bater na raquete, retornar em direçao contraria
-function colideRaquete(){
 
-    if (xBolinha - raioBolinha < xRaquete + larguraRaquete && yBolinha - raioBolinha < yRaquete + alturaRaquete && yBolinha + raioBolinha > yRaquete){
+function criaraquete(xraquete, yraquete, larguraraquete, alturaraquete) {
+    fill("blue"); 
+    rect(xraquete, yraquete, larguraraquete, alturaraquete);
+}
+
+function movimentaraquete() {
+    if(keyIsDown(UP_ARROW)) {
+        yraquete -= 10;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        yraquete += 10;
+    }
+}
+
+function colideraquete() {
+
+    if(xBolinha - raioBolinha < xraquete + larguraraquete && yBolinha - raioBolinha < yraquete + alturaraquete && yBolinha + raioBolinha > yraquete){
 
         xvelocidadeBolinha *= -1;
     }
-    }
+}
