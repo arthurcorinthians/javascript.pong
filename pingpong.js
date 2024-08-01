bc6698a8738eb4558d89ab3a36d041219d4f5f9c
arthurcorinthians <168776336+arthurcorinthians@users.noreply.github.com>
2024-08-01 15:48:17 -0300

    .

--- pingpong.js
+++ pingpong.js
@@ -2,6 +2,7 @@
 let xBolinha = 300;
 let yBolinha = 200;
 let tamBolinha = 25;
+let raioBolinha = tamBolinha/2;
 
 //config velocidade bolinha
 let xvelocidadeBolinha = 6;
@@ -28,7 +29,7 @@ function draw(){
     //chamando a função mov bolinha
     moveBolinha();
     //chamando a borda
-    Borda();
+    bolinhaBorda();
  
     //criandoRaquete
     criaRaquete (xRaquete, yRaquete, larguraRaquete, alturaRaquete);
